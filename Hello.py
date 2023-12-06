61fc1aa5ef0bf8d88d72a2e719d81a4b5f6dcc85
kj <kumudjoseph@gmail.com>
2023-12-06 12:55:08 +0530

    Update Hello.py
    modified to dr mix chatbot

--- Hello.py
+++ Hello.py
@@ -1,82 +1,83 @@
 import streamlit as st
-import re
-import yake
-from multi_rake import Rake
-from keybert import KeyBERT
-from keyphrase_vectorizers import KeyphraseCountVectorizer
-from transformers.pipelines import pipeline
-from flair.embeddings import TransformerDocumentEmbeddings, WordEmbeddings, DocumentPoolEmbeddings
-from better_profanity import profanity
-#import spacy
+import random
+import time
+import pandas as pd
+import numpy as np
+import math
+import chromadb
+from chromadb.utils import embedding_functions
+import csv
+import uuid
+#openpyxl
 
-#loading landguage models
-kw_model3 = KeyBERT(model='all-mpnet-base-v2')
-hf_model = pipeline(task = "feature-extraction", model="distilbert-base-cased")
-kw_model4 = KeyBERT(model=hf_model)
-#spacy_model = spacy.load("en_core_web_md", exclude=['tagger', 'parser', 'ner', 'attribute_ruler', 'lemmatizer'])
-#kw_model5 = KeyBERT(model=spacy_model)
-# header
-st.title(':blue[Keyphrase Extractor - Qodequay]')
+data = pd.read_excel('Dr Mix Chatbot Q&A.xlsx', sheet_name='Sheet1')
+data_x=data[['Question','Answer']].drop_duplicates().to_dict('records')
+#print(data.head())
 
-# Model input
-model = st.selectbox(
-    'Select a model for keyphrase extraction',
-    ('KW_Gen1', 'KW_Gen2','KW_Gen3','KW_Gen4'))
+questions = []
+questions.extend(data['Question'])
+#print(len(questions))
 
-def clear_text():
-    st.session_state["text"] = ""
+# Remove 'nan' values
+nan = "nan"
+questions = [item for item in questions if not isinstance(item, float) or not math.isnan(item)]
 
-# Paragraph input
-input_paragraph = st.text_area("Input Paragraph: ",key="text")
-st.button("Clear", on_click=clear_text)
-max_words = st.number_input("Max words in a keyphrase: ", min_value=1, max_value=4, step=1, value=2)
-no_of_keywords = st.number_input("Number of keyprases required: ", min_value=3, max_value=12, step=1, value = 6)
-input_paragraph = re.sub(r"[^a-zA-Z0-9 ]", "", input_paragraph)
-input_paragraph = profanity.censor(input_paragraph,'')
+# Print the cleaned list
+#print(questions)
 
-if input_paragraph:
-    if model == 'KW_Gen1':
-        kw_extractor = yake.KeywordExtractor(lan="en", n=max_words, dedupLim=0.5, dedupFunc="seqm", windowsSize=1, top=no_of_keywords, features=None, stopwords=None)
-        keywords = kw_extractor.extract_keywords(input_paragraph)
-        kw_list = []
-        for kw, v in keywords:
-            kw_list.append(kw)
-        # Display the keywords
-        st.write("Keyphrases List: ", " ; ".join(kw_list))
-    if model == 'KW_Gen2':
-        rake = Rake(max_words=max_words)
-        keywords = rake.apply(input_paragraph)
-        kw_list = []
-        for kw, v in keywords[:no_of_keywords]:
-            kw_list.append(kw)
-        # Display the keywords
-        st.write("Keyphrases List: ", " ; ".join(kw_list))
-    if model == 'KW_Gen3':
-        keywords = kw_model3.extract_keywords(input_paragraph, top_n=no_of_keywords, keyphrase_ngram_range=(1, max_words), diversity=0.5, use_mmr=True, stop_words="english",highlight=False)
-        kw_list = []
-        for kw, v in keywords[:no_of_keywords]:
-            kw_list.append(kw)
-        # Display the keywords
-        st.write("Keyphrases List: ", " ; ".join(kw_list))
-    if model == 'KW_Gen4':
-        keywords = kw_model4.extract_keywords(input_paragraph, top_n=no_of_keywords, keyphrase_ngram_range=(1, max_words), diversity=0.5, use_mmr=True, stop_words="english",highlight=False)
-        kw_list = []
-        for kw, v in keywords[:no_of_keywords]:
-            kw_list.append(kw)
-        # Display the keywords
-        st.write("Keyphrases List: ", " ; ".join(kw_list))
-    if model == 'KW_Gen5':
-        keywords = kw_model5.extract_keywords(input_paragraph, top_n=no_of_keywords, keyphrase_ngram_range=(1, max_words), diversity=0.5, use_mmr=True, stop_words="english",highlight=False)
-        kw_list = []
-        for kw, v in keywords[:no_of_keywords]:
-            kw_list.append(kw)
-        # Display the keywords
-        st.write("Keyphrases List: ", " ; ".join(kw_list))           
-    if model == 'KW_Gen6':
-        keywords = kw_model6.extract_keywords(input_paragraph, top_n=no_of_keywords, keyphrase_ngram_range=(1, max_words), diversity=0.5, use_mmr=True, stop_words="english",highlight=False)
-        kw_list = []
-        for kw, v in keywords[:no_of_keywords]:
-            kw_list.append(kw)
-        # Display the keywords
-        st.write("Keyphrases List: ", " ; ".join(kw_list))
+id_lst = [uuid.uuid4().hex for i in range(0,len(questions))]
+#print(id_lst)
 
+
+huggingface_ef = embedding_functions.HuggingFaceEmbeddingFunction(
+    api_key="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",
+    model_name="sentence-transformers/multi-qa-distilbert-cos-v1"
+)
+
+chroma_client = chromadb.Client()
+collection = chroma_client.get_or_create_collection(name="dr_questions_data")
+#print(collection)
+
+collection.add(
+    documents=questions,
+    ids=id_lst
+)
+
+def chat_engine_response(search_query,vector_db,stored_data_dict):
+    results = collection.query(query_texts=[user_prompt],n_results=1)
+    question = results['documents'][0][0]
+    for row in stored_data_dict:
+        if row['Question'] == question:
+            response = row['Answer']
+            return response
+
+st.title("Dr Mix Virtual Assitant")
+
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Welcome to Dr Mix, how can I help you"}]
+
+
+# Display chat messages to screen
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+
+# get user prompt
+user_prompt = st.chat_input()
+
+# check is user typed in something
+if user_prompt is not None:
+    st.session_state.messages.append({"role": "user", "content": user_prompt})
+    with st.chat_message("user"):
+        st.write(user_prompt)
+
+# Get the last message, if not AI(assistant), generate LLM response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Loading..."):
+            ai_response = chat_engine_response(user_prompt,collection,data_x)
+            st.write(ai_response)
+    new_ai_message = {"role": "assistant", "content": ai_response}
+    st.session_state.messages.append(new_ai_message)
